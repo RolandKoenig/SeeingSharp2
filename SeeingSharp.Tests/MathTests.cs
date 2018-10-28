20068ea733905a8713e58ce30b7a032e86473e1e
Roland König <roland.koenig@rolandk.de>
2018-10-28 12:35:43 +0100

    Integrated Unittests from Seeing#

---
+++ SeeingSharp.Tests/MathTests.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharpDX;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeeingSharp.Tests
+{
+    [TestClass]
+    public class MathTests
+    {
+        public const string TEST_CATEGORY = "SeeingSharp Mathematics";
+
+        [TestMethod]
+        [TestCategory(TEST_CATEGORY)]
+        public void Movemenet_SpeedBased()
+        {
+            MovementSpeed palletConveyorSpeed = new MovementSpeed(0.3f);
+            MovementSpeed trayConveyorSpeed = new MovementSpeed(0.8f);
+
+            MovementAnimationHelper animHelper1 = new MovementAnimationHelper(palletConveyorSpeed, new Vector3(5f, 0f, 0f));
+            MovementAnimationHelper animHelper2 = new MovementAnimationHelper(palletConveyorSpeed, new Vector3(3f, 4f, 2f));
+            MovementAnimationHelper animHelper3 = new MovementAnimationHelper(trayConveyorSpeed, new Vector3(5f, 0f, 0f));
+
+            Assert.IsTrue((int)animHelper1.MovementTime.TotalMilliseconds == 16667);
+            Assert.IsTrue((int)animHelper2.MovementTime.TotalMilliseconds == 17951);
+            Assert.IsTrue((int)animHelper3.MovementTime.TotalMilliseconds == 6250);
+        }
+
+        [TestMethod]
+        [TestCategory(TEST_CATEGORY)]
+        public void Movement_SpeedBased_FixedTime()
+        {
+            Vector3 movementVector = new Vector3(10f, 0f, 8f);
+            MovementSpeed movementSpeed = new MovementSpeed(movementVector, TimeSpan.FromSeconds(5.0));
+
+            MovementAnimationHelper animHelper = new MovementAnimationHelper(movementSpeed, movementVector);
+
+            Assert.IsTrue(animHelper.MovementTime == TimeSpan.FromSeconds(5.0));
+            Assert.IsTrue(animHelper.AccelerationTime == TimeSpan.Zero);
+            Assert.IsTrue(animHelper.DecelerationTime == TimeSpan.Zero);
+        }
+
+        [TestMethod]
+        [TestCategory(TEST_CATEGORY)]
+        public void Movement_AccDecFull_ShortRun()
+        {
+            // Configuration
+            MovementSpeed movementSpeed = new MovementSpeed(1.5f, 0.8f, -0.4f);
+            Vector3 movementVector = new Vector3(0f, 0f, -0.14f);
+
+            // Action
+            MovementAnimationHelper animHelper = new MovementAnimationHelper(movementSpeed, movementVector);
+
+            Vector3 pos = animHelper.GetPartialMoveDistance(animHelper.MovementTime);
+
+            // Asserts
+            Assert.IsTrue(animHelper.AccelerationTime > TimeSpan.Zero);
+            Assert.IsTrue(animHelper.DecelerationTime > TimeSpan.Zero);
+            Assert.IsTrue(animHelper.DecelerationTime > animHelper.AccelerationTime);
+            Assert.IsTrue(animHelper.FullSpeedTime == TimeSpan.Zero);
+            Assert.IsTrue(animHelper.MovementTime < TimeSpan.FromSeconds(15.0));
+        }
+
+        [TestMethod]
+        [TestCategory(TEST_CATEGORY)]
+        public void Movement_AccDecFull_LongRun()
+        {
+            // Configuration
+            MovementSpeed movementSpeed = new MovementSpeed(1.5f, 0.6f, -0.5f);
+            Vector3 movementVector = new Vector3(10f, 0f, 8f);
+
+            // Action
+            MovementAnimationHelper animHelper = new MovementAnimationHelper(movementSpeed, movementVector);
+
+            // Asserts
+            Assert.IsTrue(animHelper.AccelerationTime > TimeSpan.Zero);
+            Assert.IsTrue(animHelper.DecelerationTime > TimeSpan.Zero);
+            Assert.IsTrue(animHelper.DecelerationTime > animHelper.AccelerationTime);
+            Assert.IsTrue(animHelper.FullSpeedTime > animHelper.AccelerationTime);
+            Assert.IsTrue(animHelper.FullSpeedTime > animHelper.DecelerationTime);
+            Assert.IsTrue(animHelper.MovementTime < TimeSpan.FromSeconds(15.0));
+        }
+    }
+}
---
+++ SeeingSharp.Tests/ResourceLinkTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SeeingSharp.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeeingSharp.Tests
+{
+    [TestClass]
+    public class ResourceLinkTests
+    {
+        public const string TEST_CATEGORY = "SeeingSharp ResourceLink";
+
+        [TestMethod]
+        [TestCategory(TEST_CATEGORY)]
+        public void GetFileExtension_StandardFile()
+        {
+            ResourceLink extCS = new FileSystemResourceLink("C:/Blub/Blub.cs");
+            ResourceLink extC = new FileSystemResourceLink("C:/Blub/Blub.c");
+            ResourceLink extNull = new FileSystemResourceLink("C:/Club/Blub");
+            ResourceLink extVB = new FileSystemResourceLink("C:/Club/Blub.cs.vb");
+
+            Assert.IsTrue(extCS.FileExtension == "cs");
+            Assert.IsTrue(extC.FileExtension == "c");
+            Assert.IsTrue(extNull.FileExtension == "");
+            Assert.IsTrue(extVB.FileExtension == "vb");
+        }
+
+        [TestMethod]
+        [TestCategory(TEST_CATEGORY)]
+        public void GetFileExtension_AssemblyResourceLink()
+        {
+            ResourceLink extPNG = new AssemblyResourceLink(this.GetType(), "DummyNamespace.DummyFile.png");
+            ResourceLink extJPG = extPNG.GetForAnotherFile("Dummy.jpg");
+
+            Assert.IsTrue(extPNG.FileExtension == "png");
+            Assert.IsTrue(extJPG.FileExtension == "jpg");
+        }
+    }
+}
