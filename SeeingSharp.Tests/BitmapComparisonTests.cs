4b5fd473b05e34b3b5403e0da4e67e046c5a999e
Roland König <roland.koenig@rolandk.de>
2018-10-28 12:53:24 +0100

    Integrated BitmapComparion tests

---
+++ SeeingSharp.Tests/BitmapComparisonTests.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SeeingSharp.Tests.Util;
+using SeeingSharp.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using GDI = System.Drawing;
+
+namespace SeeingSharp.Tests
+{
+    [TestClass]
+    public class BitmapComparisonTests
+    {
+        public const string TEST_CATEGORY = "SeeingSharp Util BitmapComparison";
+
+        private GDI.Bitmap LoadBitmapFromResource(string fileName)
+        {
+            var resourceLink = new AssemblyResourceLink(
+                this.GetType().Assembly,
+                "SeeingSharp.Tests.Resources.BitmapComparison",
+                fileName);
+            using (var inStream = resourceLink.OpenRead())
+            {
+                return (GDI.Bitmap)GDI.Bitmap.FromStream(inStream);
+            }
+        }
+
+        [TestMethod]
+        [TestCategory(TEST_CATEGORY)]
+        public void BitmapComparison_Positive()
+        {
+            using (GDI.Bitmap leftBitmap = LoadBitmapFromResource("FlatShadedObject.png"))
+            using (GDI.Bitmap rightBitmap = LoadBitmapFromResource("FlatShadedObject.png"))
+            {
+                Assert.IsTrue(
+                    BitmapComparison.CalculatePercentageDifference(leftBitmap, rightBitmap) == 0f);
+            }
+        }
+
+        [TestMethod]
+        [TestCategory(TEST_CATEGORY)]
+        public void BitmapComparison_Negative_Without_Model()
+        {
+            using (GDI.Bitmap leftBitmap = LoadBitmapFromResource("ClearedScreen.png"))
+            using (GDI.Bitmap rightBitmap = LoadBitmapFromResource("FlatShadedObject.png"))
+            {
+                float comparisonResult = BitmapComparison.CalculatePercentageDifference(leftBitmap, rightBitmap);
+                Assert.IsTrue(comparisonResult > 0.25f);
+                Assert.IsTrue(comparisonResult < 0.6f);
+            }
+        }
+
+        [TestMethod]
+        [TestCategory(TEST_CATEGORY)]
+        public void BitmapComparison_Negative_Inversed_Image()
+        {
+            using (GDI.Bitmap leftBitmap = LoadBitmapFromResource("FlatShadedObject.png"))
+            using (GDI.Bitmap rightBitmap = LoadBitmapFromResource("FlatShadedObject_Negative.png"))
+            {
+                float comparisonResult = BitmapComparison.CalculatePercentageDifference(leftBitmap, rightBitmap);
+                Assert.IsTrue(comparisonResult > 0.9f);
+                Assert.IsTrue(comparisonResult <= 1.0f);
+            }
+        }
+
+        [TestMethod]
+        [TestCategory(TEST_CATEGORY)]
+        public void BitmapComparison_Negative_BlackWhite()
+        {
+            using (GDI.Bitmap leftBitmap = LoadBitmapFromResource("WhiteScreen.png"))
+            using (GDI.Bitmap rightBitmap = LoadBitmapFromResource("BlackScreen.png"))
+            {
+                float comparisonResult = BitmapComparison.CalculatePercentageDifference(leftBitmap, rightBitmap);
+                Assert.IsTrue(comparisonResult == 1.0f);
+            }
+        }
+
+        [TestMethod]
+        [TestCategory(TEST_CATEGORY)]
+        public void BitmapComparison_Negative_Enlighted()
+        {
+            using (GDI.Bitmap leftBitmap = LoadBitmapFromResource("FlatShadedObject.png"))
+            using (GDI.Bitmap rightBitmap = LoadBitmapFromResource("FlatShadedObject_Enlighted.png"))
+            {
+                float comparisonResult = BitmapComparison.CalculatePercentageDifference(leftBitmap, rightBitmap);
+                Assert.IsTrue(comparisonResult > 0.1);
+                Assert.IsTrue(comparisonResult < 0.4);
+            }
+        }
+
+        [TestMethod]
+        [TestCategory(TEST_CATEGORY)]
+        public void BitmapComparison_Negative_Smaller()
+        {
+            using (GDI.Bitmap leftBitmap = LoadBitmapFromResource("FlatShadedObject.png"))
+            using (GDI.Bitmap rightBitmap = LoadBitmapFromResource("FlatShadedObject_Smaller.png"))
+            {
+                float comparisonResult = BitmapComparison.CalculatePercentageDifference(leftBitmap, rightBitmap);
+                Assert.IsTrue(comparisonResult > 0.1);
+                Assert.IsTrue(comparisonResult < 0.4);
+            }
+        }
+    }
+}
---
+++ SeeingSharp.Tests/Util/BitmapComparison.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using GDI = System.Drawing;
+
+namespace SeeingSharp.Tests.Util
+{
+    /// <summary>
+    /// This class is for comparing two pictueres and to calculate the difference between these.
+    /// Originally, this algorithm was based on http://www.codeproject.com/Articles/374386/Simple-image-comparison-in-NET.
+    /// 
+    /// In order to apply ghe GPLv3 to this project, I reimplemented this class and made following main changes:
+    ///  - No scaling of the images to smaller representations (not needed for my use-cases)
+    ///  - No use of List of float during comparison algorithm
+    ///  - No GetPixel!
+    /// </summary>
+    public class BitmapComparison
+    {
+        private const int MAX_PER_PIXEL_DIFF = 128;
+        private const decimal MAX_PER_PIXEL_DIFF_DEC = (decimal)MAX_PER_PIXEL_DIFF;
+
+        /// <summary>
+        /// Determines whether the given bitmaps are near equality.
+        /// </summary>
+        /// <param name="bitmapLeft">The bitmap left.</param>
+        /// <param name="bitmapRight">The bitmap right.</param>
+        public static bool IsNearEqual(Bitmap bitmapLeft, Bitmap bitmapRight)
+        {
+            return CalculatePercentageDifference(bitmapLeft, bitmapRight) < 0.05;
+        }
+
+        /// <summary>
+        /// Caculate a differece between two pictures
+        /// </summary>
+        /// <param name="bitmapLeft">The first bitmap for the comparison.</param>
+        /// <param name="bitmapRight">The second bitmap for the comparison.</param>
+        /// <returns>Difference of both bitmaps in precent</returns>
+        public static float CalculatePercentageDifference(Bitmap bitmapLeft, Bitmap bitmapRight)
+        {
+            if (bitmapLeft == null) { throw new ArgumentNullException("bitmapLeft"); }
+            if (bitmapRight == null) { throw new ArgumentNullException("bitmapRight"); }
+            if (bitmapLeft.Size != bitmapRight.Size) { throw new SeeingSharpException("Both bitmaps musst have the same size!"); }
+
+            decimal totalDiffPercent = 0M;
+
+            GDI.Rectangle lockRect = new GDI.Rectangle(
+                new GDI.Point(0, 0),
+                bitmapLeft.Size);
+            BitmapData dataLeft = bitmapLeft.LockBits(lockRect, ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+            BitmapData dataRight = bitmapRight.LockBits(lockRect, ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+            try
+            {
+                unsafe
+                {
+                    byte* dataLeftP = (byte*)dataLeft.Scan0.ToPointer();
+                    byte* dataRightP = (byte*)dataRight.Scan0.ToPointer();
+                    decimal pixelCount = lockRect.Width * lockRect.Height;
+
+                    for (int loopY = 0; loopY < lockRect.Height; loopY++)
+                    {
+                        for (int loopX = 0; loopX < lockRect.Width; loopX++)
+                        {
+                            // Calculate the memory location of the current pixel
+                            int currentLoc = loopY * lockRect.Height * 4 + loopX * 4;
+
+                            // Get the maximum difference on this pixel on one color channel
+                            int diffA = Math.Abs(dataLeftP[currentLoc] - dataRightP[currentLoc]);
+                            int diffR = Math.Abs(dataLeftP[currentLoc + 1] - dataRightP[currentLoc + 1]);
+                            int diffG = Math.Abs(dataLeftP[currentLoc + 2] - dataRightP[currentLoc + 2]);
+                            int diffB = Math.Abs(dataLeftP[currentLoc + 3] - dataRightP[currentLoc + 3]);
+                            int maxDiff = Math.Max(diffA, Math.Max(diffR, Math.Max(diffG, diffB)));
+                            if (maxDiff > MAX_PER_PIXEL_DIFF) { maxDiff = MAX_PER_PIXEL_DIFF; }
+
+                            // Calculate the difference factor on this pixel and add it to the total difference value
+                            decimal pixelDiffPercent =
+                                (decimal)((decimal)maxDiff / MAX_PER_PIXEL_DIFF_DEC);
+                            totalDiffPercent += pixelDiffPercent / pixelCount;
+                        }
+                    }
+                }
+            }
+            finally
+            {
+                bitmapLeft.UnlockBits(dataLeft);
+                bitmapRight.UnlockBits(dataRight);
+            }
+
+            return (float)totalDiffPercent;
+        }
+    }
+}
