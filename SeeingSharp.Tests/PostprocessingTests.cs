24b34b8e2e9cc89ed1a8b764d29a8101b43dc30c
Roland König <roland.koenig@rolandk.de>
2018-10-29 05:30:48 +0100

    Integrated postprocessing tests

---
+++ SeeingSharp.Tests/PostprocessingTests.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SeeingSharp.Multimedia.Core;
+using SeeingSharp.Multimedia.Drawing3D;
+using SeeingSharp.Multimedia.Objects;
+using SeeingSharp.Multimedia.Views;
+using SeeingSharp.Tests.Util;
+using SeeingSharp.Util;
+using SharpDX;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using GDI = System.Drawing;
+
+namespace SeeingSharp.Tests
+{
+    [TestClass]
+    [DoNotParallelize]
+    public class PostprocessingTests
+    {
+        public const string TEST_DUMMY_FILE_NAME = "UnitTest.Screenshot.png";
+        public const string TEST_CATEGORY = "SeeingSharp Multimedia Postprocessing";
+
+        [TestMethod]
+        [TestCategory(TEST_CATEGORY)]
+        public async Task Postprocessing_Focus()
+        {
+            await TestUtilities.InitializeWithGrahicsAsync();
+
+            using (MemoryRenderTarget memRenderTarget = new MemoryRenderTarget(1024, 1024))
+            {
+                memRenderTarget.ClearColor = Color4Ex.CornflowerBlue;
+
+                // Get and configure the camera
+                PerspectiveCamera3D camera = memRenderTarget.Camera as PerspectiveCamera3D;
+                camera.Position = new Vector3(0f, 5f, -7f);
+                camera.Target = new Vector3(0f, 0f, 0f);
+                camera.UpdateCamera();
+
+                // Define scene
+                await memRenderTarget.Scene.ManipulateSceneAsync((manipulator) =>
+                {
+                    var keyPostprocess = manipulator.AddResource<FocusPostprocessEffectResource>(
+                        () => new FocusPostprocessEffectResource(false, 0f));
+
+                    SceneLayer defaultLayer = manipulator.GetLayer(Scene.DEFAULT_LAYER_NAME);
+                    defaultLayer.PostprocessEffectKey = keyPostprocess;
+
+                    NamedOrGenericKey geoResource = manipulator.AddResource<GeometryResource>(
+                        () => new GeometryResource(new CubeType()));
+
+                    GenericObject newObject = manipulator.AddGeneric(geoResource);
+                    newObject.RotationEuler = new Vector3(0f, EngineMath.RAD_90DEG / 2f, 0f);
+                    newObject.Scaling = new Vector3(2f, 2f, 2f);
+                    newObject.Color = Color4Ex.RedColor;
+                });
+                await memRenderTarget.AwaitRenderAsync();
+
+                // Take screenshot
+                GDI.Bitmap screenshot = await memRenderTarget.RenderLoop.GetScreenshotGdiAsync();
+
+                // TestUtilities.DumpToDesktop(screenshot, "Blub.png");
+
+                // Calculate and check difference
+                bool isNearEqual = BitmapComparison.IsNearEqual(
+                    screenshot, TestUtilities.LoadBitmapFromResource("Postprocessing", "PostProcess_Focus.png"));
+                Assert.IsTrue(isNearEqual, "Difference to reference image is to big!");
+            }
+
+            // Finishing checks
+            Assert.IsTrue(GraphicsCore.Current.MainLoop.RegisteredRenderLoopCount == 0, "RenderLoops where not disposed correctly!");
+        }
+
+        [TestMethod]
+        [TestCategory(TEST_CATEGORY)]
+        public async Task Postprocessing_EdgeDetect()
+        {
+            await TestUtilities.InitializeWithGrahicsAsync();
+
+            using (MemoryRenderTarget memRenderTarget = new MemoryRenderTarget(1024, 1024))
+            {
+                memRenderTarget.ClearColor = Color4Ex.CornflowerBlue;
+
+                // Get and configure the camera
+                PerspectiveCamera3D camera = memRenderTarget.Camera as PerspectiveCamera3D;
+                camera.Position = new Vector3(0f, 5f, -7f);
+                camera.Target = new Vector3(0f, 0f, 0f);
+                camera.UpdateCamera();
+
+                // Define scene
+                await memRenderTarget.Scene.ManipulateSceneAsync((manipulator) =>
+                {
+                    var keyPostprocess = manipulator.AddResource<EdgeDetectPostprocessEffectResource>(
+                        () => new EdgeDetectPostprocessEffectResource()
+                        {
+                            Thickness = 10f
+                        });
+
+                    SceneLayer defaultLayer = manipulator.GetLayer(Scene.DEFAULT_LAYER_NAME);
+                    defaultLayer.PostprocessEffectKey = keyPostprocess;
+
+                    NamedOrGenericKey geoResource = manipulator.AddResource<GeometryResource>(
+                        () => new GeometryResource(new CubeType()));
+
+                    GenericObject newObject = manipulator.AddGeneric(geoResource);
+                    newObject.RotationEuler = new Vector3(0f, EngineMath.RAD_90DEG / 2f, 0f);
+                    newObject.Scaling = new Vector3(2f, 2f, 2f);
+                    newObject.Color = Color4Ex.RedColor;
+                });
+                await memRenderTarget.AwaitRenderAsync();
+
+                // Take screenshot
+                GDI.Bitmap screenshot = await memRenderTarget.RenderLoop.GetScreenshotGdiAsync();
+
+                // TestUtilities.DumpToDesktop(screenshot, "Blub.png");
+
+                // Calculate and check difference
+                bool isNearEqual = BitmapComparison.IsNearEqual(
+                    screenshot, TestUtilities.LoadBitmapFromResource("Postprocessing", "PostProcess_EdgeDetect.png"));
+                Assert.IsTrue(isNearEqual, "Difference to reference image is to big!");
+            }
+
+            // Finishing checks
+            Assert.IsTrue(GraphicsCore.Current.MainLoop.RegisteredRenderLoopCount == 0, "RenderLoops where not disposed correctly!");
+        }
+    }
+}
