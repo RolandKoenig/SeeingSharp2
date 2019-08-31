c68da01d81cecee079b2a208aa77c3439c7b4adb
Roland König <roland.koenig@rolandk.de>
2019-08-31 11:45:37 +0200

    New project für Wpf in .Net Core 3.0

---
+++ Samples/SeeingSharp.WpfCoreSamples/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using SeeingSharp.Multimedia.Core;
+
+namespace SeeingSharp.WpfSamples
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            GraphicsCore.Loader
+                .SupportWpf()
+                .Load();
+
+            base.OnStartup(e);
+        }
+    }
+}
---
+++ SeeingSharp.WpfCore/_AssemblyAttributes.cs
@@ -0,0 +1,6 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Markup;
+
+[assembly: XmlnsDefinition("http://www.rolandk.de/seeingsharp", "SeeingSharp.Multimedia.Views")]
