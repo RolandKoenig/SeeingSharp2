1d2e4a31766cf0b6c21b72e83bdff401804bfcf6
Roland König <roland.koenig@rolandk.de>
2019-12-08 12:50:08 +0100

    Updated version info, publish as prerelease

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -12,8 +12,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Seeing# is a 3D/2D rendering library for C# powered by Direct3D. It is meant for desktop applications (Win.Forms, Wpf) or Windows Store Apps")]
 
 // Version info
-[assembly: AssemblyVersion("0.5.0.0")]
-[assembly: AssemblyFileVersion("0.5.0.0")]
+[assembly: AssemblyVersion("0.5.1.0")]
+[assembly: AssemblyFileVersion("0.5.1.0")]
 
 // Misc
 [assembly: ComVisible(false)]
--- script-publish.ps1
+++ script-publish.ps1
@@ -1,3 +1,5 @@
+Remove-Item -path ./publish/*
+
 dotnet pack -c Release -o ./publish ./SeeingSharp
 dotnet pack -c Release -o ./publish ./SeeingSharp.WinForms
 dotnet pack -c Release -o ./publish ./SeeingSharp.WinFormsCore
