653d12a6baa22c771da1b11a52dcf6e5c2f835d1
Roland König <roland.koenig@rolandk.de>
2020-01-03 06:41:00 +0100

    Updated publish settings for version 0.5.2

--- script-publish.ps1
+++ script-publish.ps1
@@ -5,3 +5,8 @@ dotnet pack -c Release -o ./publish ./SeeingSharp.WinForms
 dotnet pack -c Release -o ./publish ./SeeingSharp.WinFormsCore
 dotnet pack -c Release -o ./publish ./SeeingSharp.Wpf
 dotnet pack -c Release -o ./publish ./SeeingSharp.WpfCore
+
+dotnet publish -c Release -f net471 -r win7-x86 -o ./publish/WinFormsSamples ./Samples/SeeingSharp.WinFormsSamples
+dotnet publish -c Release -f netcoreapp3.1 -r Portable -o ./publish/WinFormsCoreSamples ./Samples/SeeingSharp.WinFormsCoreSamples
+dotnet publish -c Release -f net471 -r win7-x86 -o ./publish/WpfSamples ./Samples/SeeingSharp.WpfSamples
+dotnet publish -c Release -f netcoreapp3.1 -r Portable -o ./publish/WpfCoreSamples ./Samples/SeeingSharp.WpfCoreSamples
