79ee7f8e3af574b34e3c44256533b55363f9f89c
Roland König <roland.koenig@rolandk.de>
2020-01-12 10:05:54 +0100

    Updates statusbar in Uwp sample container

--- Samples/SeeingSharp.UwpSamples/Controls/StatusBarControl.xaml.cs
+++ Samples/SeeingSharp.UwpSamples/Controls/StatusBarControl.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -12,20 +14,54 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using SeeingSharp.Multimedia.Core;
 using SeeingSharp.Multimedia.Views;
 
 // The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
 
 namespace SeeingSharp.UwpSamples.Controls
 {
-    public sealed partial class StatusBarControl : UserControl
+    public sealed partial class StatusBarControl : UserControl, INotifyPropertyChanged
     {
         public static readonly DependencyProperty CtrlRendererProperty =
             DependencyProperty.Register(nameof(CtrlRenderer), typeof(SeeingSharpRenderPanel), typeof(StatusBarControl), new PropertyMetadata(null));
 
+        private DispatcherTimer m_refreshTimer;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
         public StatusBarControl()
         {
             this.InitializeComponent();
+
+            if (!GraphicsCore.IsLoaded) { return; }
+
+            this.Loaded += this.OnLoaded;
+            this.Unloaded += this.OnUnloaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            if(m_refreshTimer != null){ return; }
+
+            m_refreshTimer = new DispatcherTimer();
+            m_refreshTimer.Interval = TimeSpan.FromMilliseconds(500.0);
+            m_refreshTimer.Tick += this.OnRefreshTimer_Tick;
+            m_refreshTimer.Start();
+        }
+
+        private void OnUnloaded(object sender, RoutedEventArgs e)
+        {
+            if(m_refreshTimer == null){ return; }
+
+            m_refreshTimer.Stop();
+            m_refreshTimer = null;
+        }
+
+        private void OnRefreshTimer_Tick(object sender, object e)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(this.CountObjects)));
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(this.CountResources)));
         }
 
         public SeeingSharpRenderPanel CtrlRenderer
@@ -33,5 +69,9 @@ namespace SeeingSharp.UwpSamples.Controls
             get => (SeeingSharpRenderPanel)this.GetValue(CtrlRendererProperty);
             set => this.SetValue(CtrlRendererProperty, value);
         }
+
+        public int CountObjects => this.CtrlRenderer?.RenderLoop.VisibleObjectCount ?? 0;
+
+        public int CountResources => this.CtrlRenderer?.RenderLoop.ResourceCount ?? 0;
     }
 }
