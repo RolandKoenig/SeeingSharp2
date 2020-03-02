94f31097ccf396d3caaff1d5936b0697f1119ef1
Roland König <roland.koenig@rolandk.de>
2020-03-02 06:01:40 +0100

    Added small console application for inspecting object layout using ObjectLayoutInspector library

--- SeeingSharp/_Properties.cs
+++ SeeingSharp/_Properties.cs
@@ -4,3 +4,4 @@ using System.Runtime.CompilerServices;
 using System.Text;
 
 [assembly:InternalsVisibleTo("SeeingSharp.Tests")]
+[assembly:InternalsVisibleTo("SeeingSharp.ObjectLayoutInspection")]
---
+++ Tools/SeeingSharp.ObjectLayoutInspection/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ObjectLayoutInspector;
+using SeeingSharp.Multimedia.Drawing3D;
+
+namespace SeeingSharp.ObjectLayoutInspection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TypeLayout.PrintLayout<VertexBasic>();
+            TypeLayout.PrintLayout<VertexBinormalTangent>();
+
+            //TypeLayout.PrintLayout<CBPerFrame>();
+            //TypeLayout.PrintLayout<CBPerView>();
+            //TypeLayout.PrintLayout<CBPerObject>();
+            //TypeLayout.PrintLayout<CBPerMaterial>();
+        }
+    }
+}
