bfa626b002a9168149d3a3753e51d5f7b629bf90
Roland König <roland.koenig@rolandk.de>
2020-04-19 07:32:46 +0200

    Updated publish settings. Updated classic framework references to .Net 4.8

--- script-publish.ps1
+++ script-publish.ps1
@@ -1,4 +1,4 @@
-Remove-Item -path ./publish/*
+Remove-Item -Path ./publish/* -Recurse -Force
 
 dotnet pack -c Release -o ./publish ./SeeingSharp
 dotnet pack -c Release -o ./publish ./SeeingSharp.WinForms
@@ -6,7 +6,5 @@ dotnet pack -c Release -o ./publish ./SeeingSharp.WinFormsCore
 dotnet pack -c Release -o ./publish ./SeeingSharp.Wpf
 dotnet pack -c Release -o ./publish ./SeeingSharp.WpfCore
 
-dotnet publish -c Release -f net471 -r win7-x86 -o ./publish/WinFormsSamples ./Samples/SeeingSharp.WinFormsSamples
-dotnet publish -c Release -f netcoreapp3.1 -r Portable -o ./publish/WinFormsCoreSamples ./Samples/SeeingSharp.WinFormsCoreSamples
-dotnet publish -c Release -f net471 -r win7-x86 -o ./publish/WpfSamples ./Samples/SeeingSharp.WpfSamples
-dotnet publish -c Release -f netcoreapp3.1 -r Portable -o ./publish/WpfCoreSamples ./Samples/SeeingSharp.WpfCoreSamples
+dotnet publish -c Release -f netcoreapp3.1 -o ./publish/WinFormsCoreSamples ./Samples/SeeingSharp.WinFormsCoreSamples
+dotnet publish -c Release -f netcoreapp3.1 -o ./publish/WpfCoreSamples ./Samples/SeeingSharp.WpfCoreSamples
