8b7a722c53af0a73a1fb40c57ef149eb9b2ebfa2
Roland König <roland.koenig@rolandk.de>
2020-05-10 10:24:54 +0200

    Updated version to 0.6.1

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -5,14 +5,14 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("RolandK")]
 [assembly: AssemblyProduct("Seeing# 2 Graphics Engine")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyDescription("Seeing# is a 3D/2D rendering library for C# powered by Direct3D. It is meant for desktop applications (Win.Forms, Wpf) or Windows Store Apps")]
 
 // Version info
-[assembly: AssemblyVersion("0.6.0.0")]
-[assembly: AssemblyFileVersion("0.6.0.0")]
+[assembly: AssemblyVersion("0.6.1.0")]
+[assembly: AssemblyFileVersion("0.6.1.0")]
 
 // Misc
 [assembly: ComVisible(false)]
