13cff0d29969c463c72caff5ea837f3cb24756f3
Luca Wunderlich <mail@luca-wunderlich.de>
2020-08-26 19:10:31 +0200

    Move Custom FieldOfView-Calcuation back to Perspective Camera

--- SeeingSharp/Multimedia/Drawing3D/_Cameras/PerspectiveCamera3D.cs
+++ SeeingSharp/Multimedia/Drawing3D/_Cameras/PerspectiveCamera3D.cs
@@ -19,6 +19,7 @@
     You should have received a copy of the GNU Lesser General Public License
     along with this program.  If not, see http://www.gnu.org/licenses/.
 */
+using System;
 using System.Numerics;
 
 namespace SeeingSharp.Multimedia.Drawing3D
@@ -26,42 +27,26 @@ namespace SeeingSharp.Multimedia.Drawing3D
     public class PerspectiveCamera3D : Camera3DBase
     {
         // Configuration
-        //... see wide high-resolution film gauge https://en.wikipedia.org/wiki/70_mm_film, human eye focal 50 mm, default 35 mm
-        private float _filmGauge = 70; //mm
-        //... as focal length changes, the amount of the subject captured by the lens (the viewing angle) also changes. 
-        private float _focalLength = 30; //mm
         private float _aspectRatio;
-                     
-        /// <summary>
-        /// Gets or sets the FilmGauge.
-        /// </summary>
-        public float FilmGauge
-        {
-            get => this._filmGauge;
-            set
-            {
-                this._filmGauge = value;
-                this.UpdateCamera();
-            }
-        }
-
-        /// <summary>
-        /// Gets or sets the FocalLength.
-        /// </summary>
-        public float FocalLength
-        {
-            get => this._focalLength;
-            set
-            {
-                this._focalLength = value;
-                this.UpdateCamera();
-            }
-        }
+        private float _fov = (float)Math.PI / 4.0f; // default value for an aspect of 4:3 and an horizontal fov of 90°
 
         /// <summary>
         /// Gets the current aspect ratio.
         /// </summary>
         public float AspectRatio => _aspectRatio;
+
+        /// <summary>
+        /// Gets or sets the field of view value.
+        /// </summary>
+        public float FieldOfView
+        {
+            get => _fov;
+            set
+            {
+                _fov = value;
+                this.UpdateCamera();
+            }
+        }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="PerspectiveCamera3D"/> class.
@@ -104,9 +89,24 @@ namespace SeeingSharp.Multimedia.Drawing3D
                 ref position, ref target, ref upVector,
                 out viewMatrix);
             Matrix4x4Ex.CreatePerspectiveFovLH(
-                FOV.CalculateFieldOfView(this._aspectRatio, this._filmGauge, this._focalLength),
+                this._fov,
                 this._aspectRatio,
                 zNear, zFar, out projMatrix);
+        }
+
+        /// <summary>
+        /// Calculates the Field of View by focal length in respect to the current film gauge used them and update the camera
+        /// </summary>
+        /// <param name="filmGauge">physical property of photographic or motion picture film stock which defines its width - see wide high-resolution film gauge https://en.wikipedia.org/wiki/70_mm_film, human eye focal 50 mm, full frame movie 35 mm, default 70 mm</param>
+        /// <param name="focalLength">focal length in respect to the <paramref name="filmGauge"/> - as focal length changes, the amount of the subject captured by the lens (the viewing angle) also changes, default 30 mm</param>
+        public void CustomFieldOfView(float filmGauge = 70, float focalLength = 30)
+        {
+            float filmHeight = filmGauge / (float)Math.Max(this._aspectRatio, 1);
+            float filmSlope = 0.5f * filmHeight / focalLength;
+            this._fov = (180.0f / (float)Math.PI) * 2.0f * (float)Math.Atan((0.5f * filmHeight / filmSlope));
+            //...see http://www.bobatkins.com/photography/technical/field_of_view.html
+
+            this.UpdateCamera();
         }
     }
 }
