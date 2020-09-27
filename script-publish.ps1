e416deff6a912990ce577f733cb83188f9cb4be3
Roland König <roland.koenig@rolandk.de>
2020-09-27 07:37:24 +0200

    Updated build script for SeeingSharp.Uwp

--- script-publish.ps1
+++ script-publish.ps1
@@ -1,4 +1,40 @@
+# Search MSBuild...
+# MSBuild is needed for SeeingSharp.Uwp
+function GetMSBuildPath()
+{
+    $allPaths = .\_Misc\vswhere.exe -nologo -property installationPath -format value | Out-String
+	
+	$allPathsArray = $allPaths -split "`r`n" | Where { $_ -match '\S' }
+	if($allPathsArray.Length -eq 0)
+    {
+        Write-Error "No Visual Studio installed!"
+        exit
+    }
+
+    $msbVersion = "15.0"
+	$vsPath = ""
+	if($allPathsArray -isnot [system.array])
+	{
+		$vsPath = $allPathsArray
+	}
+	else
+	{
+		$vsPath = $allPathsArray[0]		
+	}
+
+    $msbPath = Join-Path $vsPath "MSBuild\Current\Bin\amd64\MSBuild.exe"
+    return $msbPath
+}
+
+# ************************** Main script
+
+# Clear previous published content
 Remove-Item -Path ./publish/* -Recurse -Force
+
+# ** Get MSBuild path (for SeeingSharp.Uwp)
+$msbuildExe = GetMSBuildPath
+Write-Output "MSBuild-Path: $msbuildExe"
+Write-Output ""
 
 # Build and pack all projects on the new project format
 dotnet pack -c Release -o ./publish ./SeeingSharp /p:ContinuousIntegrationBuild=true /p:IncludeSymbols=true /p:EmbedUntrackedSources=true -p:SymbolPackageFormat=snupkg
@@ -6,8 +42,8 @@ dotnet pack -c Release -o ./publish ./SeeingSharp.AssimpImporter /p:ContinuousIn
 dotnet pack -c Release -o ./publish ./SeeingSharp.WinForms /p:ContinuousIntegrationBuild=true /p:IncludeSymbols=true /p:EmbedUntrackedSources=true -p:SymbolPackageFormat=snupkg
 dotnet pack -c Release -o ./publish ./SeeingSharp.Wpf /p:ContinuousIntegrationBuild=true /p:IncludeSymbols=true /p:EmbedUntrackedSources=true -p:SymbolPackageFormat=snupkg
 
-# Pack UWP library (still old project format)
-# Be carefull: This command does not build. You have to execute release build manually in VisualStudio
+# Build and pack SeeingSharp.Uwp by MSBuild (still old project format)
+&$msbuildExe SeeingSharp.Uwp/SeeingSharp.Uwp.csproj /t:Build /p:Configuration=Release /p:ContinuousIntegrationBuild=true
 nuget pack SeeingSharp.Uwp/SeeingSharp.Uwp.nuspec -OutputDirectory publish
 
 # Publish sample applications
