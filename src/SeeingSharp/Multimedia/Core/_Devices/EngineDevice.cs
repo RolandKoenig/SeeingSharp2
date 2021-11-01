d2d41a32e12a5d49455d69d4f6e349ef06819f43
Roland König <roland.koenig@rolandk.de>
2021-11-01 10:19:04 +0100

    Removed license headers from sourcecode

--- src/SeeingSharp/Multimedia/Core/_Devices/EngineDevice.cs
+++ src/SeeingSharp/Multimedia/Core/_Devices/EngineDevice.cs
@@ -1,25 +1,4 @@
-﻿/*
-    SeeingSharp and all applications distributed together with it. 
-	Exceptions are projects where it is noted otherwise.
-    More info at 
-     - https://github.com/RolandKoenig/SeeingSharp2 (sourcecode)
-     - http://www.rolandk.de (the authors homepage, german)
-    Copyright (C) 2019 Roland König (RolandK)
-    
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Lesser General Public License as published
-    by the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-    
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Lesser General Public License for more details.
-
-    You should have received a copy of the GNU Lesser General Public License
-    along with this program.  If not, see http://www.gnu.org/licenses/.
-*/
-using System;
+﻿using System;
 using System.Collections.Generic;
 using SeeingSharp.Checking;
 using SeeingSharp.Util;
