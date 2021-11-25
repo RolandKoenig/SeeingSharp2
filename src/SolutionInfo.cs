bcbe215caa76133b9cddd9d0e4ee5a8a80be65fd
Roland König <roland.koenig@rolandk.de>
2021-11-25 06:51:49 +0100

    Updated version to 0.8.1

--- src/SolutionInfo.cs
+++ src/SolutionInfo.cs
@@ -5,14 +5,14 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("RolandK")]
 [assembly: AssemblyProduct("SeeingSharp 2 Graphics Engine")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyDescription("SeeingSharp is a 3D/2D rendering library for C# powered by Direct3D. It is meant for desktop applications (Win.Forms, Wpf) or Windows Store Apps")]
 
 // Version info
-[assembly: AssemblyVersion("0.8.0.0")]
-[assembly: AssemblyFileVersion("0.8.0.0")]
+[assembly: AssemblyVersion("0.8.1.0")]
+[assembly: AssemblyFileVersion("0.8.1.0")]
 
 // Misc
 [assembly: ComVisible(false)]
