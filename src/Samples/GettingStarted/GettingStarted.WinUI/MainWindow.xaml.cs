766308cdab54cb78eaaa4084aecd4252b4320fa5
Roland König <roland.koenig@rolandk.de>
2022-06-10 17:34:55 +0200

    Added GettingStarted for WinUI

---
+++ src/Samples/GettingStarted/GettingStarted.WinUI/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using SeeingSharp.Components.Input;
+using SeeingSharp.Core.Animations;
+using SeeingSharp.Drawing3D;
+using SeeingSharp.Drawing3D.Primitives;
+using SeeingSharp.Drawing3D.Resources;
+using SeeingSharp.Mathematics;
+
+namespace GettingStarted.WinUI
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        private bool _sceneLoaded;
+
+        public MainWindow()
+        {
+            this.InitializeComponent();
+
+            this.Activated += this.OnActivated;
+        }
+
+        private async void OnActivated(object sender, WindowActivatedEventArgs args)
+        {
+            if (_sceneLoaded)
+            {
+                return;
+            }
+            _sceneLoaded = true;
+
+            // Build the scene
+            var scene = this.CtrlView3D.Scene;
+            await scene.ManipulateSceneAsync(manipulator =>
+            {
+                // Create material resource
+                var resMaterial = manipulator.AddStandardMaterialResource();
+                var resGeometry = manipulator.AddResource(
+                    _ => new GeometryResource(new CubeGeometryFactory()));
+
+                // Create the mesh and animate it
+                var mesh = new Mesh(resGeometry, resMaterial);
+                mesh.Position = new Vector3(0f, 1f, 0f);
+                mesh.Color = Color4.GreenColor;
+                mesh.BuildAnimationSequence()
+                    .RotateEulerAnglesTo(new Vector3(0f, EngineMath.RAD_180DEG, 0f), TimeSpan.FromSeconds(2.0))
+                    .WaitFinished()
+                    .RotateEulerAnglesTo(new Vector3(0f, EngineMath.RAD_360DEG, 0f), TimeSpan.FromSeconds(2.0))
+                    .WaitFinished()
+                    .CallAction(() => mesh.RotationEuler = Vector3.Zero)
+                    .ApplyAndRewind();
+                manipulator.AddObject(mesh);
+            });
+
+            // Configure camera
+            var camera = this.CtrlView3D.Camera;
+            camera.Position = new Vector3(3f, 3f, 3f);
+            camera.Target = new Vector3(0f, 0.5f, 0f);
+            camera.UpdateCamera();
+
+            // Append camera behavior (this enables simple input / movement)
+            this.CtrlView3D.RenderLoop.SceneComponents.Add(new FreeMovingCameraComponent());
+        }
+    }
+}
